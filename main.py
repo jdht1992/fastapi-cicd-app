f00943ef9b9f03a9a451151518b47508938d3d11
diego herrera <diego.herrera@broxia-int.com>
2025-07-29 16:45:43 -0600

    add run test and run flake8 in ci

--- main.py
+++ main.py
@@ -9,4 +9,7 @@ async def read_root():
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, query_param: str = None):
-    return {"item_id": item_id, "query_param": query_param}
+    return {
+        "item_id": item_id, 
+        "query_param": query_param
+        }
